87ac924d19ba629ff02fd0073cfe8acdf494afa2
Jaskirat <jaskirat_mann@outlook.com>
2022-03-12 23:39:40 +0530

    Time: 136 ms (29.66%), Space: 51.7 MB (5.05%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    let hash = {};
+for(let word of words){
+    hash[word] = hash[word] ? hash[word] + 1 : 1;
+}
+    
+    console.log(hash);
+    
+    let hashEntries = Object.entries(hash);
+    hashEntries.sort((a,b)=> {
+        if(a[1] == b[1]){
+            console.log(a[0], b[0]);
+            // return 1;
+          return  a[0] > b[0] ?  1 :  -1;
+        }else {
+            return b[1] - a[1];
+        }
+
+    });
+    
+    // order by frequency
+    
+    // if frequency is same
+       //     order by chararcter
+    
+    
+    
+    console.log(hashEntries);
+return    hashEntries.slice(0, k).map(([word]) => word);
+};
+
+
+/*
+Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+Output: ["i","love"]
+
+let hash = {};
+for(let word of words){
+    hash[word] = hash[word] ? hash[word] + 1 : 1;
+}
+
+// sort the hash
+Object.enteries(hash)
+
+*/
