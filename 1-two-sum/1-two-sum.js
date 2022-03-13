6ee15f364766b2f78b88593eea434fa2d224baef
Jaskirat <jaskirat_mann@outlook.com>
2022-03-13 22:47:00 +0530

    Time: 85 ms (80.29%), Space: 47.5 MB (5.13%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,16 +5,32 @@
  */
 var twoSum = function(nums, target) {
     
-    let indexes=[];
+let targetHashMap = {};
+for (let i = 0; i <= nums.length-1; i++){
+    targetHashMap[target- nums[i]] = i;
     
-    for(let i =0; i <= nums.length - 2; i++){
-        for(let j = nums.length-1; j > i; j--){
-            if(nums[i] + nums[j] == target){
-                indexes.push(i,j);
-            }
-        }
+    if(nums[i + 1] in targetHashMap){
+        console.log(targetHashMap, nums[i +1], i);
+        return [i+1 , targetHashMap[nums[i+1]]];
     }
-    
-    return indexes;
+}
     
 };
+
+
+/*
+
+Brute force
+
+//     let indexes=[];
+    
+//     for(let i =0; i <= nums.length - 2; i++){
+//         for(let j = nums.length-1; j > i; j--){
+//             if(nums[i] + nums[j] == target){
+//                 indexes.push(i,j);
+//             }
+//         }
+//     }
+    
+//     return indexes;
+*/
